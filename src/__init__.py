0b6c48912a33a00cf982c7aa91122543d6287dd0
Corey De Lasaux <cordelster@gmail.com>
2025-06-21 18:39:22 -0700

    Refactoring, making off python  module

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Meshtastic Repeater Telemetry Daemon Package
+Copyright 2023 Corey DeLasaux <cordelster@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+"""
+
+from .version import __version__
+
+__author__ = "Corey DeLasaux"
+__email__ = "cordelster@gmail.com"
+__license__ = "GPL v3+"
+
+__all__ = ["__version__", "__author__", "__email__", "__license__"]
